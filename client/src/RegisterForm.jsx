cc7b9ca39ebf1b08757952a812becef755524b60
Melos Simeneh Mulu <85923940+melos-simeneh@users.noreply.github.com>
2023-08-21 14:10:09 -0700

    client: register form update

--- client/src/RegisterForm.jsx
+++ client/src/RegisterForm.jsx
@@ -5,56 +5,30 @@ import Row from "react-bootstrap/Row";
 
 function RegisterForm() {
   return (
-    <Form>
+    <Form className="px-4 py-2 pb-4">
       <Row className="mb-3">
-        <Form.Group as={Col} controlId="formGridEmail">
-          <Form.Label>Email</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+        <Form.Group as={Col}>
+          <Form.Control placeholder="First Name" />
         </Form.Group>
-
-        <Form.Group as={Col} controlId="formGridPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+        <Form.Group as={Col}>
+          <Form.Control type="password" placeholder="Last Name" />
         </Form.Group>
       </Row>
 
-      <Form.Group className="mb-3" controlId="formGridAddress1">
-        <Form.Label>Address</Form.Label>
-        <Form.Control placeholder="1234 Main St" />
+      <Form.Group className="mb-3">
+        <Form.Control placeholder="Username" />
       </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formGridAddress2">
-        <Form.Label>Address 2</Form.Label>
-        <Form.Control placeholder="Apartment, studio, or floor" />
+      <Form.Group className="mb-3">
+        <Form.Control placeholder="Password" />
       </Form.Group>
 
-      <Row className="mb-3">
-        <Form.Group as={Col} controlId="formGridCity">
-          <Form.Label>City</Form.Label>
-          <Form.Control />
-        </Form.Group>
-
-        <Form.Group as={Col} controlId="formGridState">
-          <Form.Label>State</Form.Label>
-          <Form.Select defaultValue="Choose...">
-            <option>Choose...</option>
-            <option>...</option>
-          </Form.Select>
-        </Form.Group>
-
-        <Form.Group as={Col} controlId="formGridZip">
-          <Form.Label>Zip</Form.Label>
-          <Form.Control />
-        </Form.Group>
-      </Row>
-
-      <Form.Group className="mb-3" id="formGridCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
+      <Form.Group className="mb-3">
+        <Form.Control placeholder="Confirm Passsword" />
       </Form.Group>
-
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
+      <div className="d-grid gap-2">
+        <Button variant="primary">Register</Button>
+      </div>
     </Form>
   );
 }
