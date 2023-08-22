932cb16c51054a735dc97babfe80177db1ea0e82
Melos Simeneh Mulu <85923940+melos-simeneh@users.noreply.github.com>
2023-08-22 09:34:22 -0700

    App title

--- client/src/FormContainer.jsx
+++ client/src/FormContainer.jsx
@@ -1,14 +1,17 @@
 import React from "react";
-import { Container, Tab, Tabs } from "react-bootstrap";
+import { Alert, Container, Tab, Tabs } from "react-bootstrap";
 import LoginForm from "./LoginForm";
 import RegisterForm from "./RegisterForm";
 
 const FormContainer = () => {
   return (
     <div
-      className="d-flex flex-grow-1 justify-content-center align-items-center backgroundImage"
+      className="d-flex flex-column flex-grow-1 justify-content-center align-items-center backgroundImage"
       style={{ height: "100vh" }}
     >
+      <Alert variant={"primary"} className="w-50 text-center py-1">
+        <h2 className="fs-bold">React Authentication App</h2>
+      </Alert>
       <Container className="border border-secondary-subtle rounded p-0 w-50 bg-white">
         <Tabs
           defaultActiveKey="login"
